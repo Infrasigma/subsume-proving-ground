5ae7106388ca58ac2e609d7c3685c4319df6ddae
Infrasigma <sourishpramanik5@gmail.com>
2026-09-05 22:23:40 +0530

    M6: remove duplicate Postgres HostPort declaration

--- internal/e2e/postgres_harness_api_test.go
+++
@@ -1,7 +0,0 @@
-package e2e
-
-// HostPort returns the loopback TCP address of the ephemeral PostgreSQL
-// instance. Callers do not need access to the underlying pgx pool.
-func (h postgresHarness) HostPort() string {
-	return h.address
-}
