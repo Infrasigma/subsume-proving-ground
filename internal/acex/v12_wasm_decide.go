8ae6c524b58c08c7c684bd8a581ae4d185aa8f46
Infrasigma <sourishpramanik5@gmail.com>
2026-09-21 22:44:01 +0530

    V12 audit: export compiled matcher for base-offset isolation

--- internal/acex/v12_wasm_decide.go
+++ internal/acex/v12_wasm_decide.go
@@ -58,9 +58,10 @@ func CompileV12DecisionWasm(pattern V11DirectedPatternArtifact, metadata []byte)
 
 	// Export decide() and memory.
 	exportSection := []byte{
-		0x02,
+		0x03,
 		0x06, 'd', 'e', 'c', 'i', 'd', 'e', 0x00, 0x01,
 		0x06, 'm', 'e', 'm', 'o', 'r', 'y', 0x02, 0x00,
+		0x0e, 'm', 'a', 't', 'c', 'h', '_', 'c', 'a', 'n', 'd', 'i', 'd', 'a', 't', 'e', 0x00, 0x00,
 	}
 	module = append(module, wasmSection(7, exportSection)...)
 
